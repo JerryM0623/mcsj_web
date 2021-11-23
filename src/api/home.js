f4a908d902fb55486933ac92d91460c02986994b
熊熊熊子路 <1711279251@qq.com>
2021-11-23 22:29:21 +0800

    删除无用代码

--- src/api/home.js
+++ src/api/home.js
@@ -3,19 +3,7 @@ import Axios from "../utils/Axios";
 function getCarousel(){
   return Axios.get('/index/RotationOne')  
 }
-function get401(){
-  return Axios.get('/index/RotationOne')  
-}
-function get403(){
-  return Axios.get('/index/RotationOne')  
-}
-function get404(){
-  return Axios.get('/index/RotationTwo432432')  
-}
 
 export default {
-  getCarousel,
-  get401,
-  get403,
-  get404
+  getCarousel
 }
