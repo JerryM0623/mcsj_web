ecf58994aa1781498ec7f177228dcccadd130c52
熊熊熊子路 <1711279251@qq.com>
2021-11-27 20:58:59 +0800

    添加路由元信息

--- src/router/index.js
+++ src/router/index.js
@@ -14,37 +14,58 @@ const routes = [
   {
     path:'/about',
     name:'about',
-    component:() => import('@/views/About')
+    component:() => import('@/views/About'),
+    meta:{
+      title:'关于星百汇'
+    }
   },
   {
     path:'/product',
     name:'product',
-    component:() => import('@/views/Product')
+    component:() => import('@/views/Product'),
+    meta:{
+      title:'产品展示'
+    }
   },
   {
     path:'/business',
     name:'business',
-    component:() => import('@/views/Business')
+    component:() => import('@/views/Business'),
+    meta:{
+      title:'商学院'
+    }
   },
   {
     path:'/consult',
     name:'consult',
-    component:() => import('@/views/Consult')
+    component:() => import('@/views/Consult'),
+    meta:{
+      title:'媒体资讯'
+    }
   },
   {
     path:'/VRshow',
     name:'VRshow',
-    component:() => import('@/views/VRshow')
+    component:() => import('@/views/VRshow'),
+    meta:{
+      title:'VR展厅'
+    }
   },
   {
     path: '/contact',
     name:'contact',
-    component:() => import('@/views/Contact')
+    component:() => import('@/views/Contact'),
+    meta:{
+      title:'联系我们'
+    }
   },
   {
     path:'/join',
     name:'join',
-    component: () => import('@/views/Join')
+    component: () => import('@/views/Join'),
+    meta:{
+      title:'加入我们'
+    }
   }
 ]
 
